d2b38bf7b837602780339066c18a56439d103376
Abhii5496 <abhishekhsharma2002@gmail.com>
2022-09-23 14:38:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import './index.css';
+// require('dotenv').config()
+const api ={
+  key:process.env.REACT_APP_API,
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState('')
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then(result =>{ 
+        setWeather(result)
+        setQuery('')
+        console.log(result);
+        })
+    }
+  }
+
+  const dateBuilder =(d) => {
+
+    let months =["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+    let days =["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+     <main>
+      <div className='search-box'>
+        <input type="text" 
+        onChange={e =>setQuery(e.target.value)} 
+        value= {query} onKeyPress={search}
+        className='search-bar' placeholder='Search...'/>
+      </div>
+
+     {(typeof weather.main != "undefined") ? (
+      <div>
+      <div className='location-box'>
+      <div className='location'>{weather.name}, {weather.sys.country}</div>
+      <div className='date'>{dateBuilder(new Date())}</div>
+      </div>
+
+      <div className='weather-box'>
+        <div className='temp'> {Math.round(weather.main.temp)}°C</div>
+        <div className='weather'>{weather.weather[0].main}</div>
+      </div>
+      </div>
+     ) : ('')}
+     </main>
     </div>
   );
 }
 
 export default App;
+
+// https://api.openweathermap.org/data/2.5weather?q=pakistan&unit=metric&APPID=a32b8578d8d64a0355fb54b7f73533b4
